109a5318eabda9213c5d0e4516b8bd4f52c4d702
Harlan Valmer <hvalmer@gmail.com>
2020-03-20 22:44:53 -0300

    Good Solution

--- src/application/ProgramPersonalExceptions.java
+++ src/application/ProgramPersonalExceptions.java
@@ -6,43 +6,44 @@ import java.util.Date;
 import java.util.Scanner;
 
 import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 public class ProgramPersonalExceptions {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 	
-		Scanner sca = new Scanner(System.in);
-		SimpleDateFormat sdf = new SimpleDateFormat("dd /MM/yyyy");
-		
-		System.out.print("Room number: ");
-		int number = sca.nextInt();
-		System.out.print("Check - in date (dd/MM/yyyy): ");
-		Date checkIn = sdf.parse(sca.next());
-		System.out.print("Check - out date (dd/MM/yyyy): ");
-		Date checkOut = sdf.parse(sca.next());
-		
-		if(!checkOut.after(checkIn)) {
-			System.out.println("Error in reservation: Check-out date must be after check-in date");
+					Scanner sca = new Scanner(System.in);
+				SimpleDateFormat sdf = new SimpleDateFormat("dd /MM/yyyy");
+				
+				try {
+					System.out.print("Room number: ");
+					int number = sca.nextInt();
+					System.out.print("Check-in date (dd/MM/yyyy): ");
+					Date checkIn = sdf.parse(sca.next());
+					System.out.print("Check-out date (dd/MM/yyyy): ");
+					Date checkOut = sdf.parse(sca.next());
+					
+					Reservation reservation = new Reservation(number, checkIn, checkOut);
+					System.out.println("Reservation: " + reservation);
+					
+					System.out.println();
+					System.out.println("Enter data to update the reservation:");
+					System.out.print("Check-in date (dd/MM/yyyy): ");
+					checkIn = sdf.parse(sca.next());
+					System.out.print("Check-out date (dd/MM/yyyy): ");
+					checkOut = sdf.parse(sca.next());
+					
+					reservation.updatesDates(checkIn, checkOut);
+					System.out.println("Reservation: " + reservation);
+				}
+		catch(ParseException exp) {
+				System.out.println("Invalid date format");
 		}
-		else {
-			Reservation reservation = new Reservation(number, checkIn, checkOut);
-			System.out.println("Reservation: " + reservation);
-			
-			//Atualizando a data de reserva
-			System.out.println();
-			System.out.println("Enter date to update the reservation: ");
-			System.out.print("Check - in date (dd/MM/yyyy): ");
-			checkIn = sdf.parse(sca.next());
-			System.out.print("Check - out date (dd/MM/yyyy): ");
-			checkOut = sdf.parse(sca.next());
-			
-			String error = reservation.updatesDates(checkIn, checkOut);
-			if(error != null) {
-				System.out.println("Error in reservation: " + error);
-			}
-			else{
-				System.out.println("Reservation: " + reservation);
-			}
+		catch(DomainException exp){
+			System.out.println("Error in reservation: " + exp.getMessage());
+		}
+		catch(RuntimeException exp) {
+			System.out.println("Unexpected error");
 		}
 		sca.close();
 	}
